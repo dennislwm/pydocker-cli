dc9b6ca4a987e851ba9d6547bf95cbd77b2e025a
Dennis Lee <dennislwm@yahoo.com.au>
2020-11-14 16:25:47 +0800

    fix(app.py): update app.py to get inputs from os environment
    if settings.yaml (.gitignore) does not exists, then get inputs from os enviroment
    
    #4

---
+++ README.md
@@ -0,0 +1,76 @@
+# pydocker-cli
+
+pydocker-cli starter project.
+
+---
+## Table of Contents
+- [pydocker-cli](#pydocker-cli)
+  - [Table of Contents](#table-of-contents)
+  - [Hypothesis](#hypothesis)
+  - [Project Structure](#project-structure)
+  - [Methodology](#methodology)
+    - [Non-existant viable feature](#non-existant-viable-feature)
+    - [Existing viable feature](#existing-viable-feature)
+  - [Complexity](#complexity)
+    - [Non-existant bloat](#non-existant-bloat)
+    - [Existing bloat](#existing-bloat)
+    - [References](#references)
+  - [Contributing](#contributing)
+    - [Reach Out!](#reach-out)
+
+---
+## Hypothesis
+**pydocker-cli** was a personal project to:
+* make lots of half-baked tiny python command line apps
+
+---
+## Project Structure
+     pydocker-cli/                                <-- Root of your project
+       |- .gitignore                              <-- Git ignore file
+       |- README.md                               <-- This README markdown file
+       +- htmltopdf/                              <-- Source files for Html link to PDF conversion
+       +- udemyenrol/                             <-- Source files for Udemy free course
+          |- .dockerignore                        <-- Docker ignore file
+          |- actions.yml                          <-- GitHub actions file for app
+          |- app.py                               <-- Tiny Python app
+          |- Dockerfile                           <-- Dockerfile for app
+          |- requirements.txt                     <-- Tiny Python app dependencies
+
+---
+## Methodology
+
+This is the minimum viable product (MVP) to test the above hypothesis.
+
+### Non-existant viable feature
+* make lots of half-baked tiny python command line apps
+
+### Existing viable feature
+
+* send email from Gmail using smtplib (Rade2020)
+* continuous deployment of tiny python apps using GitHub Actions (Mezz2020)
+
+---
+## Complexity
+
+Count the cost of complexity, i.e. incremental reward and risk reduction, before evolving MVP.
+
+### Non-existant bloat
+* Nil
+
+### Existing bloat 
+* Nil
+
+---
+### References
+- (Rade2020) Dano Radecic, 2-Nov-2020, [How to Send Beautiful Emails With Python — The Essential Guide](https://towardsdatascience.com/how-to-send-beautiful-emails-with-python-the-essential-guide-a01d00c80cd0)
+- (Mezz2020) David Mezzetti, 14-Nov-2020, [GitHub Actions For the Win](https://towardsdatascience.com/github-actions-for-the-win-8a215d390c1b)
+
+---
+## Contributing
+
+Please read the [contributing guide](https://github.com/dennislwm/pydocker-cli/blob/master/CONTRIBUTING.md) on how you can actively participate in the development of this repository.
+
+---
+### Reach Out!
+
+Please consider giving this repository a star on GitHub.
---
+++ udemyenrol/app.py
@@ -0,0 +1,236 @@
+# Install all the requirements by running requirements.py in IDLE or follow the alternate instructions at
+# https://github.com/aapatre/Automatic-Udemy-Course-Enroller-GET-PAID-UDEMY-COURSES-for-FREE/ Make sure you have
+# cleared all saved payment details on your Udemy account & the browser!
+import time
+import os
+from datetime import datetime
+from multiprocessing.dummy import Pool
+
+import requests
+# Gmail setup: https://myaccount.google.com/apppasswords
+import smtplib
+from email.message import EmailMessage
+from bs4 import BeautifulSoup
+from ruamel.yaml import YAML
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+settings = getConfig()
+
+email, password = settings["udemy"]["email"], settings["udemy"]["password"]
+strGmail, strGmailPassword = settings["udemy"]["gmail"], settings["udemy"]["gmail_app_password"]
+
+# Accounts for the edge case that someone removes the entire "zipcode" entry in settings.yaml instead of simply clearing the string or leaving it alone
+# This shouldn't have to exist, but ?? here we are
+if "zipcode" in settings["udemy"]:
+    zipcode = settings["udemy"]["zipcode"]
+
+# DL
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument("--disable-setuid-sandbox")
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+
+# Maximizes the browser window since Udemy has a responsive design and the code only works
+driver.maximize_window()
+# in the maximized layout
+
+def getConfig():
+    #-------------------------
+    # Read from os environment
+    config["udemy"]["email"] = ""
+    config["udemy"]["password"] = ""
+    config["udemy"]["gmail"] = os.environ['GITHUB_GMAIL']
+    config["udemy"]["gmail_app_password"] = os.environ['GITHUB_GMAIL_APP_PASSWORD']
+
+    #--------------------------------------
+    # A YAML file supercedes os environment
+    if os.path.exists("settings.yaml"):
+        yaml = YAML()
+        with open("settings.yaml") as f:
+            config = yaml.load(f)
+        
+    return config
+    
+
+def getUdemyLink(url):
+    response = requests.get(url=url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    linkForUdemy = soup.find("span",
+                             class_="rh_button_wrapper").find("a").get("href")
+
+    return linkForUdemy
+
+
+def gatherUdemyCourseLinks(courses):
+    """
+    Threaded fetching of the udemy course links from tutorialbar.com
+    :param list courses: A list of tutorialbar.com course links we want to fetch the udemy links for
+    :return: list of udemy links
+    """
+    thread_pool = Pool()
+    results = thread_pool.map(getUdemyLink, courses)
+    thread_pool.close()
+    thread_pool.join()
+    return results
+
+
+def getTutorialBarLinks(url):
+    response = requests.get(url=url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    links = soup.find("div", class_="rh-post-wrapper").find_all("a")
+    # print(links)
+
+    courses = []
+
+    x = 0
+    for i in range(12):
+        courses.append(links[x].get("href"))
+        x = x + 3
+
+    return courses
+
+
+def udemyLogin(email_text, password_text):
+    driver.get("https://www.udemy.com/join/login-popup/")
+
+    userEmail = driver.find_element_by_name("email")
+    userPassword = driver.find_element_by_name("password")
+
+    userEmail.send_keys(email_text)
+    userPassword.send_keys(password_text)
+
+    driver.find_element_by_name("submit").click()
+
+
+def redeemUdemyCourse(url):
+    driver.get(url)
+    print("Trying to Enroll for: " + driver.title)
+
+    # Enroll Now 1
+    element_present = EC.presence_of_element_located(
+        (By.XPATH, "//button[@data-purpose='buy-this-course-button']"))
+    WebDriverWait(driver, 10).until(element_present)
+
+    udemyEnroll = driver.find_element_by_xpath(
+        "//button[@data-purpose='buy-this-course-button']")  # Udemy
+    udemyEnroll.click()
+
+    # Enroll Now 2
+    element_present = EC.presence_of_element_located((
+        By.XPATH,
+        '//*[@class="udemy pageloaded"]/div[1]/div[2]/div/div/div/div[2]/form/div[2]/div/div[4]/button',
+    ))
+    WebDriverWait(driver, 10).until(element_present)
+
+    # Check if zipcode exists before doing this
+    if zipcode:
+        # Assume sometimes zip is not required because script was originally pushed without this
+        try:
+            zipcode_element = driver.find_element_by_id(
+                "billingAddressSecondaryInput")
+            zipcode_element.send_keys(zipcode)
+
+            # After you put the zip code in, the page refreshes itself and disables the enroll button for a split second.
+            time.sleep(1)
+        except NoSuchElementException:
+            pass
+
+    udemyEnroll = driver.find_element_by_xpath(
+        '//*[@class="udemy pageloaded"]/div[1]/div[2]/div/div/div/div[2]/form/div[2]/div/div[4]/button'
+    )  # Udemy
+    udemyEnroll.click()
+
+
+def file_open(strPath="./"):
+    filename = strPath + 'udemyenrol.md'
+    # mode: (a)ppend, over(w)rite
+    f = open(filename, 'w')
+    return f
+
+def file_write(f, strLine):
+    f.write(strLine + '\n\r')
+
+def file_close(f):
+    f.close()
+
+def send_gmail(strBody):
+    now = datetime.now()
+    strSubject = "Daily Free Udemy Courses - " + now.strftime("%d/%m/%Y")
+    print("Send email " + strSubject) 
+    msg = EmailMessage()
+    msg['Subject'] = strSubject
+    msg['From'] = strGmail
+    msg['To'] = strGmail
+    msg.set_content(strBody)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(strGmail, strGmailPassword)
+        smtp.send_message(msg)
+
+def main_function():
+    page = 1  # Change the page number here only if necessary, else ignore
+
+    loop_run_count = 0
+    strBody = ""
+
+    f = file_open("./")
+    while True:
+
+        print("Please Wait: Getting the course list from tutorialbar.com...")
+        print("Page: " + str(page) + ", Loop run count: " +
+              str(loop_run_count))
+
+        url = "https://www.tutorialbar.com/all-courses/" + "page/" + str(
+            page) + "/"
+        courses = getTutorialBarLinks(url)
+
+        udemyLinks = gatherUdemyCourseLinks(courses)
+
+        for counter, course in enumerate(udemyLinks):
+            print("Received Link {} : {}".format((counter + 1), course))
+            # output to text file
+            if loop_run_count == 0:
+                file_write(f, course)
+                strBody = strBody + course + "\r\n"
+
+        loop_run_count = loop_run_count + 1
+        # output to text file
+        if loop_run_count == 1:
+            file_close(f)
+            send_gmail(strBody)
+            break
+
+"""
+        if loop_run_count == 0:
+            udemyLogin(email, password)
+
+        for link in udemyLinks:
+            # noinspection PyBroadException
+            try:
+                redeemUdemyCourse(link)
+            except BaseException as e:
+                print(
+                    "Unable to enroll for this course either because you have already claimed it or the browser "
+                    "window has been closed!")
+
+        page = page + 1
+        loop_run_count = loop_run_count + 1
+
+        print(
+            "Moving on to the next page of the course list on tutorialbar.com")
+"""
+
+
+main_function()
